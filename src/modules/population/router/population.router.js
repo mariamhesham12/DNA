85f399e02eaa435ebd294479746e5b2d3d15e1b2
mariamhesham12 <99356287+mariamhesham12@users.noreply.github.com>
2024-04-18 22:54:09 +0200

    hope it works

--- src/modules/population/router/population.router.js
+++ src/modules/population/router/population.router.js
@@ -17,8 +17,7 @@ router.get('/identification',authenticate,autherize('admin','technical'),asyncHa
 router.put('/updatePopulation/:id',authenticate,autherize('admin'),getTextFileUploadMiddleware(),validate(updatePopulationSchema,updatePopulationQuery), asyncHandler(updatePopulation));
 router.post('/identificationByDNA',authenticate,autherize('admin','technical'),getTextFileUploadMiddleware(),asyncHandler(identificationByDNA))
 /////////////////////////////////////////////////////////////////////////////////
-
-///////////////////////////////////test////////////////////////////////
+/////////////////////////////////test////////////////////////////////
 router.post('/adddpopulation',authenticate,autherize('technical'),getTextFileUploadMiddleware(),validate(AddPopulationnSchema,AddPopulationnlQuery), asyncHandler(adddPopulation));
 router.get('/EisaAPI',asyncHandler(getAllPopulationDecrypted))
 
